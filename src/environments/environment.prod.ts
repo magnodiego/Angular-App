06d1cdb639a68478b5027836e42d4676f5e8e4a2
Diego Magno <diga.magno@hotmail.com>
2020-08-23 16:09:45 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://d7d96c7ad96b.ngrok.io' // update with the link of the server
+
+  // ngrok.exe http 4200 -host-header="localhost:4200"
+};
